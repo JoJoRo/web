681050538c2d7a6081d7f2e40c3f07d850519525
J J H R <jjhernandezronquillo@gmail.com>
2017-03-27 16:32:24 +0200

    Create mail_send.php

---
+++ mail_send.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="client-nojs" lang="en" dir="ltr">
+<head>
+<meta charset="UTF-8"/>
+<title>Internet protocol suite - Wikipedia</title>
+
+  </head>
+  <body>
+	  <?php
+		$to = 'destination@server.whatever';
+		$subject = 'Sent from 000webhost';
+		$body = 'Email sent from 000webhost with PHP';
+		$headers = 'From: user@000webhost.com';
+		mail($to, $subject, $body, $headers);
+	  ?>
+  </body>
+</html>
